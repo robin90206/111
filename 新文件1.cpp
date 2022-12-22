79f562785bbab6729a6afb6256d347b28a98028f
111 <C109154203@nkust.edu.tw>
2022-12-22 14:41:53 +0800

    一 次

---
+++ 新文件1.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+main()
+{
+      int i,s;
+      s=0;
+      for(i=0;i<=100;i++)
+      s=s+i;
+      printf("1+2+3+...+100=%d\n",s);
+      system("pause");
+      }
